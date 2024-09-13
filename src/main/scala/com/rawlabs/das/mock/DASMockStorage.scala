293bea0ce3db0a3f948d84dab5d02e9338358796
Alex Zerntev <alex.zerntev@raw-labs.com>
2024-09-13 12:27:22 +0300

    indentation

--- src/main/scala/com/rawlabs/das/mock/DASMockStorage.scala
+++ src/main/scala/com/rawlabs/das/mock/DASMockStorage.scala
@@ -8,16 +8,19 @@ class DASMockStorage(
     val key: String,
     private val treeMap: mutable.TreeMap[String, Row] = new mutable.TreeMap[String, Row]()
 ) {
-
   def add(row: Row): Row = {
     treeMap.put(row.getDataMap.get(key).getInt.getV.toString, row)
     row
   }
+
   def update(rowId: String, row: Row): Row = {
     treeMap.put(rowId, row)
     row
   }
+
   def remove(rowId: String): Unit = treeMap.remove(rowId)
+
   def size: Int = treeMap.size
+
   def iterator: Iterator[Row] = treeMap.valuesIterator
 }
